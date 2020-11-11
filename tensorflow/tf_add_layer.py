c6328e67711b94909f137f65aa721502ccc82cad
LittleWhite6 <954720059@qq.com>
2020-11-11 17:01:26 +0800

    V1.0

--- tensorflow/tf_add_layer.py
+++ tensorflow/tf_add_layer.py
@@ -1,12 +1,22 @@
 import tensorflow as tf
+import numpy as np
+
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size])) #Weights矩阵形式最好首字母大写
+    Weights = tf.Variable(tf.random_normal(
+        [in_size, out_size]))  # Weights矩阵形式最好首字母大写
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs,Weights) + biases
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_function is None:
         outputs = Wx_plus_b
     else:
         outputs = activation_function(Wx_plus_b)
     return outputs
 
+
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+l1 = add_layer(x_data,1,10,activation_function=tf.nn.relu)
+predition = add_layer(l1)
