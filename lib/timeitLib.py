6f667823cfe7d3832bd6c14921e3e25a7cce5cbe
LittleWhite6 <954720059@qq.com>
2020-12-28 13:36:21 +0800

    reset

--- lib/timeitLib.py
+++
@@ -1,5 +0,0 @@
-#性能度量
-from timeit import Timer
-print(Timer('t=a; a=b; b=t','a=1; b=2').timeit())
-print(Timer('a,b=b,a', 'a=1; b=2').timeit())
-#相对于timeti的细粒度， :mod:profile和pstats模块提供了针对更大代码块的时间度量工具
--- tensorflow/tf_argmax.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-test = np.array([
-[1, 2, 3],
- [2, 3, 4], 
- [5, 4, 3], 
- [8, 7, 2]])
-np.argmax(test, 0)　　　#输出：array([3, 3, 1]
-np.argmax(test, 1)　　　#输出：array([2, 2, 0, 0]
